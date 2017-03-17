e760d23306a976d937c6e851b7dae8700fc69321
Alexey Shmalko <rasen.dubi@gmail.com>
2017-03-17 22:58:48 +0200

    Add batch handling, multiple improvements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate mqtt;
+extern crate serde_json;
 extern crate rs_jsonpath;
 #[macro_use]
 extern crate clap;
@@ -6,24 +7,26 @@ extern crate itertools;
 
 use std::net::{TcpListener, TcpStream};
 use std::thread;
-use std::rc::Rc;
-use std::fs::{File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io::{self, Write};
 use std::sync::{Arc, Mutex};
 
 use itertools::Itertools;
 
 use mqtt::{Encodable, Decodable};
-use mqtt::packet::{ConnackPacket, Packet, PingrespPacket, SubackPacket, VariablePacket};
+use mqtt::packet::{ConnackPacket, Packet, PingrespPacket, PubackPacket, PubrecPacket, PubcompPacket, SubackPacket, VariablePacket};
 use mqtt::{TopicFilter, QualityOfService};
-use mqtt::control::{ConnectReturnCode};
-use mqtt::packet::suback::{SubscribeReturnCode};
+use mqtt::control::ConnectReturnCode;
+use mqtt::packet::suback::SubscribeReturnCode;
+use mqtt::packet::publish::QoSWithPacketIdentifier;
 
-use rs_jsonpath::lookup;
+use serde_json::Value;
+
+use rs_jsonpath::look;
 
 macro_rules! log(
     ($($arg:tt)*) => { {
-        writeln!(&mut ::std::io::stderr(), $($arg)*)
+        let _ = writeln!(&mut ::std::io::stderr(), $($arg)*);
     } }
 );
 
@@ -36,7 +39,7 @@ fn main() {
 
     let jsonpaths: Vec<String> = matches.value_of("JSONPATH").unwrap().split(",").map(String::from).collect();
 
-    let mut handle: Arc<Mutex<Write + Send>> = if let Some(filepath) = matches.value_of("file") {
+    let handle: Arc<Mutex<Write + Send>> = if let Some(filepath) = matches.value_of("file") {
         Arc::new(Mutex::new(OpenOptions::new()
                  .append(true)
                  .create(true)
@@ -46,7 +49,7 @@ fn main() {
         Arc::new(Mutex::new(io::stdout())) as Arc<Mutex<Write + Send>>
     };
 
-    let listener = TcpListener::bind("127.0.0.1:1883").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:1883").unwrap();
 
     log!("Listening on port 1883");
 
@@ -56,7 +59,7 @@ fn main() {
                 let paths = jsonpaths.clone();
                 let handle = handle.clone();
                 thread::spawn(move || {
-                    handle_client(stream, paths, handle);
+                    handle_client(stream, &paths, handle);
                 });
             }
             Err(e) => {
@@ -74,15 +77,33 @@ fn sub_to_ack(&(_, qos): &(TopicFilter, QualityOfService)) -> SubscribeReturnCod
     }
 }
 
-fn handle_client(mut stream: TcpStream, jsonpaths: Vec<String>, handle: Arc<Mutex<Write>>) {
+fn process_entries(entries: &Vec<Value>, jsonpaths: &Vec<String>, handle: Arc<Mutex<Write>>) {
+    for entry in entries.into_iter() {
+        let mut csv = jsonpaths.iter()
+            .map(|path| {
+                let res = look(&entry, &entry, path.clone()).unwrap_or("".to_string());
+                if res != "[]" { res } else { "".to_string() }
+            })
+            .join(",");
+        csv.push('\n');
+
+        let mut handle = handle.lock().unwrap();
+        let _ = handle.write_all(csv.as_bytes());
+    }
+}
+
+fn handle_client(mut stream: TcpStream, jsonpaths: &Vec<String>, handle: Arc<Mutex<Write>>) {
     // This makes .read() call blocking.
     // Otherwise, mqtt decode consumes 100% CPU time.
-    stream.set_read_timeout(None);
+    let _ = stream.set_read_timeout(None);
 
     log!("{:?}", stream);
-    let auto_decode = VariablePacket::decode(&mut stream);
-    log!("{:?}", auto_decode);
-    ConnackPacket::new(false, ConnectReturnCode::ConnectionAccepted).encode(&mut stream);
+    if let Ok(VariablePacket::ConnectPacket(x)) = VariablePacket::decode(&mut stream) {
+        log!("{:?}", x);
+        ConnackPacket::new(false, ConnectReturnCode::ConnectionAccepted).encode(&mut stream);
+    } else {
+        return;
+    }
 
     loop {
         let parse_result = VariablePacket::decode(&mut stream);
@@ -99,20 +120,45 @@ fn handle_client(mut stream: TcpStream, jsonpaths: Vec<String>, handle: Arc<Mute
                     PingrespPacket::new()
                         .encode(&mut stream);
                 }
+                VariablePacket::PubrelPacket(x) => {
+                    PubcompPacket::new(x.packet_identifier())
+                        .encode(&mut stream);
+                }
                 VariablePacket::PublishPacket(x) => {
+                    match x.qos() {
+                        QoSWithPacketIdentifier::Level0 => {
+                            // No additionl handling required
+                        }
+                        QoSWithPacketIdentifier::Level1(pkid) => {
+                            PubackPacket::new(pkid)
+                                .encode(&mut stream);
+                        }
+                        QoSWithPacketIdentifier::Level2(pkid) => {
+                            PubrecPacket::new(pkid)
+                                .encode(&mut stream);
+                        }
+                    }
+
                     if let Ok(payload) = std::str::from_utf8(x.payload()) {
                         log!("{}: {}", x.topic_name(), payload);
+                    } else {
+                        log!("{}: {:?}", x.topic_name(), x.payload());
+                    }
 
-                        let mut csv = jsonpaths.iter()
-                            .map(|path| {
-                                let res = lookup(String::from(payload), path.clone()).unwrap_or("".to_string());
-                                if res != "[]" { res } else { "".to_string() }
-                            })
-                            .join(",");
-                        csv.push('\n');
-
-                        let mut handle = handle.lock().unwrap();
-                        handle.write_all(csv.as_bytes());
+                    match serde_json::from_slice(x.payload()) {
+                        Ok(Value::Object(obj)) => {
+                            if let Some(&Value::Array(ref entries)) = obj.get("entries") {
+                                process_entries(entries, jsonpaths, handle.clone());
+                            } else {
+                                // TODO: error handling
+                            }
+                        }
+                        Ok(Value::Array(entries)) => {
+                            process_entries(&entries, jsonpaths, handle.clone());
+                        }
+                        _ => {
+                            log!("Parse error");
+                        }
                     }
                 }
                 _ => {
